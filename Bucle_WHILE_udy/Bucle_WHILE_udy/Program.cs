8671ef160e100db22a0b6d146d0d2d364380c3e7
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-08 16:40:21 -0600

    Add files via upload

---
+++ Bucle_WHILE_udy/Bucle_WHILE_udy/Program.cs
@@ -0,0 +1,17 @@
+﻿//While, ejecutar accion mientras sea verdadera
+int cont = 0;
+while(cont<=10){
+     Console.WriteLine(cont);
+        cont++;
+}
+//ejemplo 2
+var balance = 200m;
+var interes = 1.07m;
+var contadorInteres = 1;
+
+while (contadorInteres <= 10)
+{   //balance x interes
+    balance *= interes;
+    contadorInteres++;
+}
+Console.WriteLine("El balance es: "+ balance);
